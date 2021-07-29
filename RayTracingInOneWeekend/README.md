5c6b26c58acef82ad98b5e17d89be4f0ece4c6ba
pzxbc <pzxbc@qq.com>
2021-07-29 11:44:45 +0800

    完成图片输出与查看

---
+++ RayTracingInOneWeekend/README.md
@@ -0,0 +1,13 @@
+# RayTracingInOneWeekend
+
+## 学习材料
+
+https://raytracing.github.io/books/RayTracingInOneWeekend.html
+
+## Timeline
+
+### 2021-07-29
+
+终于完成第一步，PPM格式的显示了...原来教程中输出的是`P3`格式，但是找了各种查看器(XnView, IrfanView...)都不支持`P3`格式了。自己重新改为了`P6`格式。终于完成第一步，图片输出与查看了！
+
+![ppm-p6.png](./attachments/ppm-p6.png)
---
+++ RayTracingInOneWeekend/code/ImageOutput/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdint.h>
+
+int main() {
+	// Image
+
+	const int image_width = 256;
+	const int image_height = 256;
+
+	std::ofstream wf("image.ppm", std::ios::out | std::ios::binary);
+	if (!wf)
+	{
+		std::cout << "open file failed!" << std::endl;
+		return 1;
+	}
+
+	// write header P6
+	wf.write("P6\n", 3);
+	auto img_width_str = std::to_string(image_width);
+	wf.write(img_width_str.c_str(), img_width_str.length());
+	wf.write(" ", 1);
+	auto img_height_str = std::to_string(image_height);
+	wf.write(img_height_str.c_str(), img_height_str.length());
+	wf.write("\n", 1);
+	wf.write("255\n", 4);
+
+	for (int j = image_height - 1; j >= 0; --j)
+	{
+		for (int i = 0; i < image_width; ++i)
+		{
+			auto r = double(i) / (image_width - 1);
+			auto g = double(j) / (image_height - 1);
+			auto b = 0.25;
+
+			uint8_t ur = static_cast<uint8_t>(255.999 * r);
+			uint8_t ug = static_cast<uint8_t>(255.999 * g);
+			uint8_t ub = static_cast<uint8_t>(255.999 * b);
+
+			/*std::cout << ir << ' ' << ig << ' ' << ib << '\n';*/
+			wf.write((const char*)(&ur), 1);
+			wf.write((const char*)(&ug), 1);
+			wf.write((const char*)(&ub), 1);
+		}
+	}
+
+	wf.close();
+
+
+}
