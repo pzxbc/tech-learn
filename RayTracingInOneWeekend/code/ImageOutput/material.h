99dffe5d3b2fccb42b9a2aa3eba1c829847d475b
pzxbc <pzxbc@qq.com>
2021-08-03 17:26:54 +0800

    fuzzed metal

--- RayTracingInOneWeekend/code/ImageOutput/material.h
+++ RayTracingInOneWeekend/code/ImageOutput/material.h
@@ -34,16 +34,17 @@ public:
 
 class Metal : public Material {
 public:
-	Metal(const Color &a) : albedo(a) {}
+	Metal(const Color &a, double f) : albedo(a), fuzz(f<1? f: 1) {}
 
 	virtual bool scatter(const Ray &r_in, const HitRecord &rec, Color &attenuation, Ray &scattered) const override
 	{
 		Vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-		scattered = Ray(rec.p, reflected);
+		scattered = Ray(rec.p, reflected + fuzz*random_in_unit_sphere());
 		attenuation = albedo;
 
 		return (dot(scattered.direction(), rec.normal) > 0);
 	}
 public:
 	Color albedo;
+	double fuzz;
 };
