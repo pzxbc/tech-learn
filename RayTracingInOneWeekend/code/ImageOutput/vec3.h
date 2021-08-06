53ac2a0d99f822c62122499c6d2142c688e0b13f
pzxbc <pzxbc@qq.com>
2021-08-06 13:05:01 +0800

    depth of field

--- RayTracingInOneWeekend/code/ImageOutput/vec3.h
+++ RayTracingInOneWeekend/code/ImageOutput/vec3.h
@@ -156,3 +156,13 @@ Vec3 random_in_hemisphere(const Vec3& normal)
 		return -in_unit_sphere;
 	}
 }
+
+Vec3 random_in_unit_disk()
+{
+	while (true)
+	{
+		auto p = Vec3(random_double(-1, 1), random_double(-1, 1), 0);
+		if (p.length_squared() >= 1) continue;
+		return p;
+	}
+}
